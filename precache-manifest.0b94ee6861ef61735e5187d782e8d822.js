de550ba2ac029ab653b6a171bce24094659cd2aa
VrajKD <vrajkishan.dugar2017@vitstudent.ac.in>
2019-09-16 19:06:42 +0530

    Updates

---
+++ precache-manifest.0b94ee6861ef61735e5187d782e8d822.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f82e7ecb686ffdae4afe017b9cabe5c",
+    "url": "/robofriends-redux-pwa/index.html"
+  },
+  {
+    "revision": "b5eed217f9fdf3a201b5",
+    "url": "/robofriends-redux-pwa/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "0b0a9bd9b0d3bd8920d9",
+    "url": "/robofriends-redux-pwa/static/css/main.e528f420.chunk.css"
+  },
+  {
+    "revision": "b5eed217f9fdf3a201b5",
+    "url": "/robofriends-redux-pwa/static/js/2.9622b0e7.chunk.js"
+  },
+  {
+    "revision": "0b0a9bd9b0d3bd8920d9",
+    "url": "/robofriends-redux-pwa/static/js/main.4b85d56a.chunk.js"
+  },
+  {
+    "revision": "66f9f225538a0202ab16",
+    "url": "/robofriends-redux-pwa/static/js/runtime~main.c410ba44.js"
+  },
+  {
+    "revision": "f4189bc4c1cae3091ea80db76799c509",
+    "url": "/robofriends-redux-pwa/static/media/astrolyt.f4189bc4.woff"
+  }
+]);
